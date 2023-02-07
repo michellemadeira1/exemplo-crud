407697693a98eac6eb1f4b200f91e205db4235a8
Michelle Madeira <michellemadeira1@gmail.com>
2023-02-07 13:49:11 -0300

    crud

---
+++ src/main/java/com/teste/exemplocrud/ExemploCrudApplication.java
@@ -0,0 +1,13 @@
+package com.teste.exemplocrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExemploCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExemploCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teste/exemplocrud/ExemploCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teste.exemplocrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExemploCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
