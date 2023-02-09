0486c5ea71d51f8468fa773466df02a8581c98a2
Michelle Madeira <michellemadeira1@gmail.com>
2023-02-09 13:13:18 -0300

    tratamento de erros - Handler

---
+++ src/main/java/handler/RestExceptionHandler.java
@@ -0,0 +1,22 @@
+package handler;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import error.ErrorMessage;
+import exception.ResourceNotFoundException;
+
+@ControllerAdvice
+public class RestExceptionHandler {
+
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public ResponseEntity<?> handleResourceNotFoundException(ResourceNotFoundException ex){
+		
+		ErrorMessage error = new ErrorMessage ("Not found", HttpStatus.NOT_FOUND.value() , ex.getMessage());
+		return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+	}
+		
+	
+}
--- src/main/java/repository/ProdutoRepository.java
+++ src/main/java/repository/ProdutoRepository.java
@@ -1,12 +1,12 @@
 package repository;
 
 import java.util.ArrayList;
-import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.stereotype.Repository;
 
+import exception.ResourceNotFoundException;
 import model.Produto;
 
 @Repository
@@ -14,39 +14,37 @@ public class ProdutoRepository {
 
 	private List<Produto> produtos = new ArrayList<Produto>();
 	private Integer ultimoId = 0;
-	
-	public List<Produto> obterTodos(){
+
+	public List<Produto> obterTodos() {
 		return produtos;
 	}
-	
+
 	public Optional<Produto> obterPorId(Integer id) {
-		return produtos
-				.stream()        
-				.findFirst();
+		return produtos.stream().findFirst();
 	}
-	
+
 	public Produto adicionar(Produto produto) {
 		ultimoId++;
 		produto.setId(ultimoId);
 		produtos.add(produto);
 		return produto;
 	}
-	
+
 	public void deletar(Integer id) {
-		produtos.remove(id);                             
+		produtos.remove(id);
 	}
-	
+
 	public Produto atualizar(Produto produto) {
-	Optional<Produto>produtoEncontrado = obterPorId(produto.getId());
-	
-	if(produtoEncontrado.isEmpty()) {
-		throw new InputMismatchException("Produto não encontrado");
-	}
+		Optional<Produto> produtoEncontrado = obterPorId(produto.getId());
+
+		if (produtoEncontrado.isEmpty()) {
+			throw new ResourceNotFoundException("Produto não encontrado");
+		}
 		deletar(produto.getId());
-		
+
 		produtos.add(produto);
-		
+
 		return produto;
-		
+
 	}
 }
